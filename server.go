d2a43a3699c60fdafc47b093af72aed9c52f9788
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2022-09-02 00:42:03 -0300

    Config objects

--- server.go
+++ server.go
@@ -1,12 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Template!!!</h1>"))
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, my name is %s and I am %s years old", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("/go/config/config.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	fmt.Fprintf(w, "My env variables", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
 }
